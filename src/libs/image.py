146efb0c76be9030e11ef6852c664c9204c731e1
Beamlnwza <Supawit04591@pcccr.ac.th>
2023-06-03 10:44:10 +0700

    add image processing libs

---
+++ src/libs/image.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generate_index(index, reloaded_model):
+    noise = np.random.normal(0, 1, (1,))
+    label = np.array([[index]])
+    return reloaded_model.predict([noise, label])[0]
+
+
+def save_img_png(image, output_path):
+    plt.imshow(image, cmap='gray')
+    plt.axis('off')
+    plt.grid(False)
+    plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
+    plt.close()
