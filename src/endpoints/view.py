ad8cac673ea956d03f93b53d8460ec60592bf439
Beamlnwza <Supawit04591@pcccr.ac.th>
2023-06-03 17:10:05 +0700

    view method for all

--- src/endpoints/view.py
+++ src/endpoints/view.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
+from src.libs.s3 import s3resource
+from uuid import UUID
+
 router = APIRouter(
     prefix="/view",
     tags=["View"],
@@ -7,11 +10,16 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def info():
-    return {"info": "This is the generate endpoint"}
-
-
 @router.get("/status")
 async def status():
     return {"status": "OK"}
+
+
+@router.get("/")
+async def view(user: UUID, index: int | None = None):
+    s3 = s3resource()
+    bucket = s3.Bucket('pimthaigans-image-container')
+    objs = list(bucket.objects.filter(Prefix=f'{user}/'))
+    if index:
+        return objs[index].key
+    return [obj.key for obj in objs]
