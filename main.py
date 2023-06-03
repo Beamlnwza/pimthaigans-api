65c7212191e95062f4a5163d2f311a869d53d025
Beamlnwza <Supawit04591@pcccr.ac.th>
2023-06-03 16:28:00 +0700

    ensure to have src/store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.api import router as api_router
+import os
 
 app = FastAPI()
 
@@ -13,5 +14,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+IMAGE_STORE_PATH = os.path.abspath("./src/store")
+
+if not os.path.exists(IMAGE_STORE_PATH):
+    os.makedirs(IMAGE_STORE_PATH)
 
 app.include_router(api_router)
